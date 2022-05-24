b85428ddd79ebb7e0a13afdd3c09d85d3adf2baf
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2022-05-24 14:47:09 +0300

    9.8

--- Osa9/First_TS/index.ts
+++ Osa9/First_TS/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { isUndefined } from 'util';
 import { parseArguments, calculateBmi } from './calculateBmi';
 import { parseHours, calculateExercises } from './exerciseCalculator';
 const app = express();
@@ -15,23 +16,37 @@ app.get('/bmi', (req, res) => {
     const msg = calculateBmi(height, weight);
     res.json({ weight, height, bmi: msg });
   } catch (err: unknown) {
-    if (err instanceof Error) res.json({ error: err.message });
-    res.json({ error: 'Something Bad Happened' });
+    if (err instanceof Error) res.status(400).json({});
+    else res.status(500).json({ error: 'Something Bad Happened' });
   }
 });
 
 app.post('/exercises', (req, res) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  const exercises = parseHours(req.body.exercises as string[]);
-  const target = req.body.target;
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (!Array.isArray(req.body.daily_exercises as string[]))
+      throw new Error('Malformatted parameters');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (isUndefined(req.body.target as number))
+      throw new Error('Malformatted parameters');
 
-  res.send('Yeah');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const exercises = parseHours(req.body.daily_exercises as string[]);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const target = Number(req.body.target);
+    if (isNaN(target)) throw Error('Target must be number');
+    const result = calculateExercises(exercises, target);
+    res.json(result);
+  } catch (err: unknown) {
+    if (err instanceof Error) res.json({ error: err.message });
+    else res.json({ error: 'Something Bad Happened' });
+  }
 });
 
 app.get('/hello', (_req, res) => {
   res.send('<h1>Hello Full Stack!</h1>');
 });
 
-app.listen(3001, () => {
+app.listen(3002, () => {
   console.log('Server running');
 });
