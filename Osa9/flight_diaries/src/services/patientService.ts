cee33bf61f42bc854590cc705dd8c9d7ca5eb973
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2022-05-25 10:49:09 +0300

    9.13

--- Osa9/flight_diaries/src/services/patientService.ts
+++ Osa9/flight_diaries/src/services/patientService.ts
@@ -1,8 +1,18 @@
 import patientData from '../../data/patiens.json';
-import { Patient, safePatient } from '../../types';
+import { Patient, safePatient, newPatient } from '../../types';
+import { v1 as uuid } from 'uuid';
 
-const patiens: Patient[] = patientData;
+let patiens: Patient[] = patientData;
 
 export const getPatients = (): safePatient[] => {
-  return patiens;
+  return patiens.map(({ ssn, ...rest }) => ({
+    ...rest,
+  }));
+};
+
+export const addPatient = (patient: newPatient): Patient => {
+  const patientToAdd = { ...patient, id: uuid() };
+  patiens.push(patientToAdd);
+
+  return patientToAdd;
 };
